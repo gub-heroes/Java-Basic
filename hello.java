ade2d9a6cbaecf1b514074d8434ddc751b12c115
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-08 10:11:16 +0600

    Add output in comment

--- hello.java
+++ hello.java
@@ -1,8 +1,14 @@
 public class hello {
   public static void main(String[] args) {
     int year = 2020;
+
     System.out.println("Hello World !!");
     System.out.print("This is " + year +" year ." );
-    //System.out.print(year);
+    
+    /* Output:
+        Hello World !!
+        This is 2020 year.    
+    */
+
   }
 }
