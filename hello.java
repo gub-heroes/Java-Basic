f7d37900b0908370aa02a1f799c9d59da264c1d2
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-08 09:48:08 +0600

    Print Hello World

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+  public static void main(String[] args) {
+    int year = 2020;
+    System.out.println("Hello World !!");
+    System.out.print("This is " + year +" year ." );
+    //System.out.print(year);
+  }
+}
