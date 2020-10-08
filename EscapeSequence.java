b7e62319d07d71d10341f0707b75ce0886666540
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-08 22:07:06 +0600

    EscapeSequence File added

---
+++ EscapeSequence.java
@@ -0,0 +1,21 @@
+public class EscapeSequence {
+    
+    /*
+    Here the escape sequence of Java . 
+    It's a best thing to try it, if you wanna escape something on your code.
+*/
+    public static void main(String[] args){
+        int myID = 193902024;
+
+        System.out.println("My name is \"MD.Shawon\"\nMy ID is : " + myID);
+        System.out.println("I\' m in CSE Dept.\nCheckout my github \"Sayed-Anawer\".\n");
+        System.out.println("\t <<<<<< So let\'s do some amazing >>>>>>\n");
+        System.out.println("\t \t \t\t  _ _");
+        System.out.println("\t|\t - -   \\      /  |");
+        System.out.println("\t|\t|   |   \\    /   |_ _");
+        System.out.println("\t|\t|   |    \\  /    |");
+        System.out.println("\t|\t|   |     \\/     |_ _");
+        System.out.println("\t _ _ _\t - - \t\n");
+
+    }
+}
