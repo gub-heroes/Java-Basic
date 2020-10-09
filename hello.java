e2cfe4f1f8ca88cb92604e242d219f76fbb10067
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-09 23:52:25 +0600

    refactor: Added output in comment.

--- hello.java
+++ hello.java
@@ -3,12 +3,12 @@ public class hello {
     int year = 2020;
 
     System.out.println("Hello World !!");
-    System.out.print("This is " + year +" year ." );
-    
-    /* Output:
-        Hello World !!
-        This is 2020 year.    
-    */
+    System.out.println("This is " + year + " year .");
+    System.out.print(year + " is memorable year for all.");
+
+    /*
+     * Output: Hello World !! This is 2020 year.
+     */
 
   }
 }
