2cfd2ea4f7ad261430c9073d4eade7a2ad046176
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-09 23:18:44 +0600

    Added float dataType and comment

--- DataType.java
+++ DataType.java
@@ -1,16 +1,16 @@
-import javax.sound.midi.SysexMessage;
-
 public class DataType {
 
     public static void main(String[] args){
+        // Output intialization
         int x = 20 ;
         char y = 'A';
-       // float z = 4.00;
-        double a = 5.00;
-        
+        float z = 3.5f;
+        double a = 5.;
+
+        // Output shown for every variables.
         System.out.println("int x = "+ x);
         System.out.println("char y = "+ y);
+        System.out.println("float z = "+ z);
         System.out.println("double a = "+ a);
     }
-
 }
