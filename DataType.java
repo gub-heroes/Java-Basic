d42e42f83290f98ee85a2ea585c1d0613a0539a1
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-10 00:04:03 +0600

    refactor: Added String dataType and initialize the value

--- DataType.java
+++ DataType.java
@@ -3,12 +3,14 @@ public class DataType {
     public static void main(String[] args){
         // Output intialization
         int x = 20 ;
+        String s = "Practise more and more to prepare you :)";
         char y = 'A';
         float z = 3.5f;
         double a = 5.;
 
         // Output shown for every variables.
         System.out.println("int x = "+ x);
+        System.out.println("Stirng s = "+s);
         System.out.println("char y = "+ y);
         System.out.println("float z = "+ z);
         System.out.println("double a = "+ a);
