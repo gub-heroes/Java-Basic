ba5a5d9d908fc0df3a73c903a62af260e2933569
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-10 19:24:21 +0600

    refactor: format the code.

--- ProblemSolve/TriangleOfCircle.java
+++ ProblemSolve/TriangleOfCircle.java
@@ -1,24 +1,24 @@
 package ProblemSolve;
 
-import java.util.Scanner;   // Import the java scanner class which is a package
+import java.util.Scanner; // Import the java scanner class which is a package
 
 public class TriangleOfCircle {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double base,hieght,area;            // Variables declare.
+        double base, hieght, area; // Variables declare.
 
-        Scanner input = new Scanner(System.in);     // Create Scanner Keyword.
+        Scanner input = new Scanner(System.in); // Create Scanner Keyword.
 
-        System.out.print("Enter base : ");      // Read user Input.
+        System.out.print("Enter base : "); // Read user Input.
         base = input.nextDouble();
 
-        System.out.print("Enter height : ");    //Read user Input.
+        System.out.print("Enter height : "); // Read user Input.
         hieght = input.nextDouble();
 
-        area = 0.5 * base * hieght;     // Area of Triangle formula.
+        area = 0.5 * base * hieght; // Area of Triangle formula.
 
-        System.out.println("Area : " + area);     // Show the Result.
+        System.out.println("Area : " + area); // Show the Result.
 
     }
 }
