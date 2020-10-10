be7df6441226bc9ab595b3611782c00b7c6ba39e
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-10 19:22:21 +0600

    refactor: Show the output and Changes to the code are commentout

--- ProblemSolve/TriangleOfCircle.java
+++ ProblemSolve/TriangleOfCircle.java
@@ -1,20 +1,24 @@
 package ProblemSolve;
 
-import java.util.Scanner;
+import java.util.Scanner;   // Import the java scanner class which is a package
 
 public class TriangleOfCircle {
 
     public static void main(String[] args){
-        double base,hieght,area;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter base : ");
+
+        double base,hieght,area;            // Variables declare.
+
+        Scanner input = new Scanner(System.in);     // Create Scanner Keyword.
+
+        System.out.print("Enter base : ");      // Read user Input.
         base = input.nextDouble();
-        System.out.print("Enter height : ");
+
+        System.out.print("Enter height : ");    //Read user Input.
         hieght = input.nextDouble();
-        area = 0.5 * base * hieght;
-        System.out.println("Area : " + area);
+
+        area = 0.5 * base * hieght;     // Area of Triangle formula.
+
+        System.out.println("Area : " + area);     // Show the Result.
 
     }
-    
-    
 }
