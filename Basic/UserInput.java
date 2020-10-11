9624b5d33c487bd4b60ae620ec49f254c38e77ff
Sayed-Anawer <sayedanawer99@gmail.com>
2020-10-11 06:13:49 +0600

    fix: input never closed.

--- Basic/UserInput.java
+++ Basic/UserInput.java
@@ -1,38 +1,42 @@
 package Basic;
+
 import java.util.Scanner; // Import the java scanner class which is a package
 
 public class UserInput {
 
     public static void main(String[] args) {
-        /*
-         * Variable declare
-         */
-        String oName, oMail;
-        int oAge;
 
         /*
          * Create Scanner object
          */
         Scanner input = new Scanner(System.in);
-        Scanner input1= new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
         /*
          * Read user input
          */
         System.out.print("Enter User name : ");
-        oName = input.nextLine();
+        String oName = input.nextLine();// Variable declare and collect input.
 
         System.out.print("Enter your age : ");
-        oAge = input.nextInt();
+        int oAge = input.nextInt();// Variable declare and collect input.
+
+        System.out.print("Enter your Cgpa : ");
+        double cgpa = input.nextDouble();// Variable declare and collect input.
 
         System.out.print("Enter your Email : ");
-        oMail = input1.nextLine();
+        String oMail = input1.nextLine();// Variable declare and collect input.
 
         /**
          * Show user given input
          */
+        System.out.println("\n<<<< Here is your basic information >>>>\n");
         System.out.println("Your user name is : " + oName);
         System.out.println("Your age is : " + oAge);
+        System.out.println("Your Cgpa is : " + cgpa);
         System.out.println("Your Email is : " + oMail);
+
+        input.close();
+        input1.close();
 
     }
 }
--- ProblemSolve/TriangleOfCircle.java
+++ ProblemSolve/TriangleOfCircle.java
@@ -8,17 +8,18 @@ public class TriangleOfCircle {
 
         double base, hieght, area; // Variables declare.
 
-        Scanner input = new Scanner(System.in); // Create Scanner Keyword.
+        Scanner gskill = new Scanner(System.in); // Create Scanner Keyword.
 
         System.out.print("Enter base : "); // Read user Input.
-        base = input.nextDouble();
+        base = gskill.nextDouble();
 
         System.out.print("Enter height : "); // Read user Input.
-        hieght = input.nextDouble();
+        hieght = gskill.nextDouble();
 
         area = 0.5 * base * hieght; // Area of Triangle formula.
 
         System.out.println("Area : " + area); // Show the Result.
 
+        gskill.close();
     }
 }
