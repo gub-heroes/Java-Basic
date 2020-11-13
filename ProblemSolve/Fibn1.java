831abd3a8219d0a56f0a0c364687a79570c283eb
Sayed-Anawer <sayedanawer99@gmail.com>
2020-11-13 16:42:52 +0600

    Fix: for loop value fixed

--- ProblemSolve/Fibn1.java
+++ ProblemSolve/Fibn1.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
  */
 public class Fibn1 {
     public static void main(String[] args){
-        int f1= 0,f2= 1,f3 = 0, n, i;
+        int f1= 0,f2= 1,f3=0, n, i;
         Scanner input = new Scanner(System.in);
         System.out.print("Decalare th:");
         n = input.nextInt();
-        for(i=0;i<n-2;i++){
+        for(i=0;i<n-1;i++){
             f3 = f1 + f2;
             f1 =f2;
             f2 =f3;
