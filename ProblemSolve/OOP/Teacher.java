ddba3f61f2b9e075ba15aaffa3d2cd7e994124b2
Sayed-Anawer <sayedanawer99@gmail.com>
2020-12-07 20:43:45 +0600

    Added a Method with perameter :)

--- ProblemSolve/OOP/Teacher.java
+++ ProblemSolve/OOP/Teacher.java
@@ -4,7 +4,12 @@ public class Teacher {
     String name;
     int phone;
 
-    void myMethod() {
+    void myMethod(String n, int ph) {
+        name = n;
+        phone = ph;
+    }
+
+    void myMethod2(){
         System.out.println("Name: "+name+"\nPhone: "+phone);
     }
 }
--- ProblemSolve/OOP/Test1.java
+++ ProblemSolve/OOP/Test1.java
@@ -3,15 +3,14 @@ package ProblemSolve.OOP;
 public class Test1 {
     public static void main(String[] args){
         Teacher teacher1 = new Teacher();
-        teacher1.name = "MD.Shawon";
-        teacher1.phone = 1995166628;
-
-        teacher1.myMethod(); 
+        teacher1.myMethod("MD.Shawon",1995166628);
+        
+        teacher1.myMethod2();
         
         Teacher teacher2 = new Teacher();
-        teacher2.name = "MD.Jonaed Hasan";
-        teacher2.phone = 1854597526;
-        teacher2.myMethod();
+        teacher2.myMethod("MD.Jonaed Hasan",1854597526);
+        
+        teacher2.myMethod2();
     }
     
 }
