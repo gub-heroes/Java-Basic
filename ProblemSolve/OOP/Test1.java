1e847c5c532fb5ac5939a24f7e34215b05ef04e5
Sayed-Anawer <sayedanawer99@gmail.com>
2020-12-07 16:28:17 +0600

    Related Class file added

---
+++ ProblemSolve/OOP/Test1.java
@@ -0,0 +1,17 @@
+package ProblemSolve.OOP;
+
+public class Test1 {
+    public static void main(String[] args){
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "MD.Shawon";
+        teacher1.phone = 1995166628;
+
+        teacher1.myMethod(); 
+        
+        Teacher teacher2 = new Teacher();
+        teacher2.name = "MD.Jonaed Hasan";
+        teacher2.phone = 1854597526;
+        teacher2.myMethod();
+    }
+    
+}
