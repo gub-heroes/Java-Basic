26ad64a06f613423dc6dafb1e0c0bc45bb24273e
Sayed-Anawer <sayedanawer99@gmail.com>
2020-12-07 16:27:29 +0600

    TeacherFile Added

---
+++ ProblemSolve/OOP/Teacher.java
@@ -0,0 +1,10 @@
+package ProblemSolve.OOP;
+
+public class Teacher {
+    String name;
+    int phone;
+
+    void myMethod() {
+        System.out.println("Name: "+name+"\nPhone: "+phone);
+    }
+}
